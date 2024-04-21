d3e517644160dfd3eff973569f19d749f89a4b83
Xharless <carlosarevalo1702@hotmail.com>
2024-04-21 19:04:56 -0400

    Actualizacion de interfaz y funcionalidad de abrir

--- Visualizador/vizualizador.py
+++ Visualizador/vizualizador.py
@@ -1,16 +1,28 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+import pydicom
+from PIL import Image, ImageTk
+
+def open_file():
+    file_path = filedialog.askopenfilename(title="Seleccionar archivo DICOM", filetypes=[("Archivos DICOM", "*.dcm")])
+    if file_path:
+        dicom_data = pydicom.dcmread(file_path)
+        image_array = dicom_data.pixel_array
+        image = Image.fromarray(image_array)
+        image_tk = ImageTk.PhotoImage(image)
+        label_image.configure(image=image_tk)
+        label_image.image = image_tk 
 
 def menubar_shortcut(event=None):
     menubar = Menu()
     filemenu = Menu(menubar, tearoff=False)
     viewmenu = Menu(menubar, tearoff=False)
     open_recent = Menu(viewmenu)
-    
     #menu de archivo
     menubar.add_cascade(label='Archivo', menu=filemenu)
     filemenu.add_command(label="Nuevo...")
-    filemenu.add_command(label="Abrir...")
+    filemenu.add_command(label="Abrir...", command=open_file)
     filemenu.add_command(label="Salir")
     
     #menu de las vistas
@@ -27,10 +39,13 @@ def menubar_shortcut(event=None):
     app.config(menu=menubar)
 
 app = Tk()
-app.geometry("800x600+560+240")
 app.title("VISUALIZADOR USM")
-app.configure(bg="lightgrey")
-
+fondo = PhotoImage(file="C:\\Users\\carlo\\OneDrive\\Escritorio\\usm\\1er semestre 2024\\Analisis\\proyecto\\GRP-LosAnalistas-2024-PROYINF\\Visualizador\\IMG\\foto_fondo.png")
+label_fondo = Label(app, image=fondo)
+label_fondo.place(x=0, y=0, relwidth=1, relheight=1)
+app.iconbitmap("C:\\Users\\carlo\\OneDrive\\Escritorio\\usm\\1er semestre 2024\\Analisis\\proyecto\\GRP-LosAnalistas-2024-PROYINF\\Visualizador\\IMG\\Foto_Logo.ico")
 menubar_shortcut()
-
+label_image = Label(app)
+label_image.pack()
+app.geometry("800x600+560+240")
 app.mainloop()
