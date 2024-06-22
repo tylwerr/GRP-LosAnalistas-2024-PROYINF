9186f71219f10d67060b176fb17515759de014f7
Romina <romi.aguilera2003@gmail.com>
2024-06-22 18:13:56 -0400

    imagen 3d

--- Visualizador/img_3d.py
+++ Visualizador/img_3d.py
@@ -4,7 +4,7 @@ import pydicom
 from mayavi import mlab
 
 #Carpeta donde estan las imagenes dicom
-directorio = r".\GRP-LosAnalistas-2024-PROYINF\DATOS_DICOM\BSSFP"
+directorio = r"..\GRP-LosAnalistas-2024-PROYINF\DATOS_DICOM\BSSFP"
 
 #Lista de los archivos dicom de la carpeta
 lista_archivos = [os.path.join(directorio, nombre_archivo) for nombre_archivo in os.listdir(directorio) if nombre_archivo.endswith('.dcm')]
@@ -18,12 +18,15 @@ for path_archivo in lista_archivos:
 
 #Se convierten las imagenes en una matriz tridimensional
 imagen_stack = np.array(imagen_stack)
+#print(imagen_stack)
 
 #Se escalan los pixeles al rango [0, 1]
 imagen_escalada_stack = imagen_stack / np.max(imagen_stack)
 
 #Se visualiza en Mayavi
+'''
 mlab.figure() #Abre Mayavi
 volumen = mlab.pipeline.volume(mlab.pipeline.scalar_field(imagen_escalada_stack), vmin=0.0, vmax=1.0) #Los parametros dan el color
 mlab.colorbar() #Opcional, sirve para saber el rango de valores de los colores
 mlab.show()
+'''
